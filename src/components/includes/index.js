4f0b8e1efa626435d8db297884eaf22cbde4eada
exroot <eliastorrealba1998@outlook.com>
2019-10-06 16:48:49 -0400

    Restructuring components path

---
+++ src/components/includes/index.js
@@ -0,0 +1,5 @@
+export { default as Loader } from './Button/Button';
+export { default as Navbar } from './Navbar/Navbar';
+export { default as Button } from './Button/Button';
+export { default as Form } from './Form/Form';
+export { default as ErrorHandler } from './ErrorHandler/ErrorHandler';
