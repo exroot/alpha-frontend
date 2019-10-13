46df57da016bb88647d8202ca72c557322e60afc
exroot <eliastorrealba1998@outlook.com>
2019-10-12 23:16:16 -0400

    Adding user authentication

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,33 +1,84 @@
 import React, {useState, useEffect } from 'react';
 import { BrowserRouter , Route, Switch } from 'react-router-dom';
-import notesServices from '../../services/notes';
+
+/*** Services ***/
+import noteServices from '../../services/notes';
 
 /*** Components ***/
-import { Navbar, Loader } from '../includes';
+import { Navbar, Loader, ErrorHandler } from '../includes';
 import AuthForm from '../auth/AuthForm';
 import { NotesLayout } from '../notes';
 
 import './App.css';
 
 const App = () => {
-
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [user, setUser] = useState(null);
+  const [loader, setLoader] = useState(true);
   const [notes, setNotes] = useState([]);
+  const [isAuth, setAuth] = useState(false);
+
+  /* Load all notes if user is valid (has logged) */
+  useEffect(() => {
+    if (user) {
+      noteServices.getAll().then(notes => {
+        setTimeout(() => {
+          setLoader(false);
+          setNotes(notes);
+        }, 5000);
+      });
+    }
+  }, [user]);
+ 
+  /* Check for changes on user status (triggered when we has a valid login via AuthForm) */
+  useEffect(() => {
+    if (user) {
+      setAuth(true);
+      window.localStorage.setItem('loggedMyNotesUser', JSON.stringify(user));
+      noteServices.setToken(user.token);
+	    setErrorMessage(null);
+    }
+  }, [user]);
+
+  /* Check localstorage for a valid item which represent our user (the 1st time our app is loaded) */
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedMyNotesUser');
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      setAuth(true);
+      setUser(user);
+      noteServices.setToken(user.token);
+    }
+  }, [])
 
   useEffect(() => {
-    notesServices.getAll().then(notes => {
-      setNotes(notes);
-    })
-  }, []);
+    if (errorMessage) {
+      /* Clear error message from our layout after timeout */
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 3500);
+    }
+  }, [errorMessage]);
+
+  const logoutHandler = () => {
+    setUser(null);
+    setAuth(null);
+    noteServices.setToken(null);
+    window.localStorage.removeItem('loggedMyNotesUser');
+  }
 
   return (
       <div className="App">
         <BrowserRouter>
-          <Navbar />
+          <Navbar isAuth={ isAuth } logoutHandler={ logoutHandler } redirect={'/login'}/>
+          { errorMessage && <ErrorHandler> { errorMessage } </ErrorHandler> }
           <Switch>
-            <Route exact path="/signin" component={AuthForm}/>
-            <Route exact path="/notes" render={(props) => <NotesLayout notes={notes} /> }/>
+            { !user ?
+              <Route exact path="/login" render={( props ) => <AuthForm {...props} setUser={ setUser } setErrorMessage={ setErrorMessage } /> } />
+            :
+              <Route exact path="/notes" render={(props) => loader ? <Loader user={user.email} /> : <NotesLayout notes={notes} /> }/>
+            }
           </Switch>
-          <Loader />
         </BrowserRouter>
       </div>
   );
--- src/components/auth/AuthForm.js
+++ src/components/auth/AuthForm.js
@@ -1,18 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Button } from '../includes';
 import InputComponent from '../includes/Form/Input/Input';
 import LabelComponent from '../includes/Form/Label/Label';
+import login from '../../services/auth';
 
-const authForm = props => {
+const AuthForm = props => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handlerLogin = async (event) => {
+        /* Prevent reload page when submitting form */
+        event.preventDefault();
+        try {
+            /*
+                Login request could have an user object inside of the data key 
+                or could be an error throwed by the service
+            */
+            const loginRequest = await login(email, password);
+            /* loginRequest.data = user object with user id, user email and token */
+            props.setUser(loginRequest.data);
+            setPassword('');
+            setEmail('');
+            props.history.push('/notes');
+        } catch (err) {
+            if (err.request.status) {
+                /* 
+                    If error has an manegable status 
+                        => evaluate cases
+                */
+                switch (err.request.status) {
+                    case 401: props.setErrorMessage("User doesn't exist."); break;
+                    case 422: props.setErrorMessage("Wrong credentials."); break;
+                    default: props.setErrorMessage("Server error"); break;
+                }
+            }
+            else
+                props.setErrorMessage(err);
+        }
+    }
     return (
-    <Form method={props.method} action={props.action}>
+    <Form onSubmit={ handlerLogin } className="centered">
         <div className="form-group" >
-            <LabelComponent for="email">Email</LabelComponent>
-            <InputComponent placeholder="email" type="email" name="email"/>
+            <LabelComponent >Email</LabelComponent>
+            <InputComponent placeholder="email" type="email" name="email" onChange={({target}) => setEmail(target.value)}/>
         </div>
         <div className="form-group" >
-            <LabelComponent for="password">Password</LabelComponent>
-            <InputComponent placeholder="password" type="password" name="password"/>
+            <LabelComponent >Password</LabelComponent>
+            <InputComponent placeholder="password" type="password" name="password" onChange={({target}) => setPassword(target.value)}/>
         </div>
         <div className="form-group">
             <p><a href="/">Forgot your password?</a></p>
@@ -21,4 +55,4 @@ const authForm = props => {
     </Form>)
 }
 
-export default authForm;
+export default AuthForm;
